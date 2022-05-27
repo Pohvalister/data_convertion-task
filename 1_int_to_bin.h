3012e6fe13c604c9f3c1090d6a26b1e89c340f62
Ivan Ignashov <i.ignashov@yandex.ru>
2022-05-27 14:36:26 +0300

    initial commit

---
+++ 1_int_to_bin.h
@@ -0,0 +1,8 @@
+#ifndef INT_TO_BIN
+#define INT_TO_BIN
+
+std::string toBin(int val){
+	return "0";
+}
+
+#endif //1_INT_TO_BIN
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "1_int_to_bin.h"
+
+int main()
+{
+	std::cout << toBin(0) << std::endl;
+	return 0;
+}
